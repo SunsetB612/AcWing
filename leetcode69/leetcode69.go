77eab91b0215bc42817b94ac271d234e8a969a4e
SunsetB612 <10235101575@stu.ecnu.edu.cn>
2026-03-31 12:09:28 +0800

    leetcode 69
    Signed-off-by: SunsetB612 <10235101575@stu.ecnu.edu.cn>

---
+++ leetcode69/leetcode69.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func mySqrt(x int) int {
+	low, high := 0, x
+
+	for low <= high {
+		mid := (low + high) / 2
+		if mid*mid == x {
+			return mid
+		} else if mid*mid < x {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return high
+}
+
+func main() {
+	fmt.Println(mySqrt(4))
+	fmt.Println(mySqrt(8))
+}
