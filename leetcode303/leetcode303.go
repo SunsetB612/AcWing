8996d0b6e5062f458473ada7947def5528bdad88
SunsetB612 <10235101575@stu.ecnu.edu.cn>
2026-04-06 15:50:42 +0800

    leetcode 303
    Signed-off-by: SunsetB612 <10235101575@stu.ecnu.edu.cn>

---
+++ leetcode303/leetcode303.go
@@ -0,0 +1,17 @@
+package main
+
+type NumArray struct {
+	prefix []int
+}
+
+func Constructor(nums []int) NumArray {
+	prefix := make([]int, len(nums)+1)
+	for i, v := range nums {
+		prefix[i+1] = prefix[i] + v
+	}
+	return NumArray{prefix}
+}
+
+func (this *NumArray) SumRange(left int, right int) int {
+	return this.prefix[right+1] - this.prefix[left]
+}
