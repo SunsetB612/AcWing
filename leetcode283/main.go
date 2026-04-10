3c6a93599be599b94367940f9b71872d93335079
SunsetB612 <10235101575@stu.ecnu.edu.cn>
2026-04-10 23:31:20 +0800

    leetcode 283
    Signed-off-by: SunsetB612 <10235101575@stu.ecnu.edu.cn>

---
+++ leetcode283/main.go
@@ -0,0 +1,16 @@
+package main
+
+func moveZeroes(nums []int) {
+	left := 0
+	for right := 0; right < len(nums); right++ {
+		if nums[right] != 0 {
+			nums[left], nums[right] = nums[right], nums[left]
+			left++
+		}
+	}
+}
+
+func main() {
+	nums := []int{0}
+	moveZeroes(nums)
+}
