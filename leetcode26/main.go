94258cf9e80ff61216d7bb511cf0c3fd24dc1375
SunsetB612 <10235101575@stu.ecnu.edu.cn>
2026-04-10 20:10:26 +0800

    leetcode26
    Signed-off-by: SunsetB612 <10235101575@stu.ecnu.edu.cn>

---
+++ leetcode26/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	k := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[k-1] {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+	return k
+}
+
+func main() {
+	nums := []int{1, 1, 2}
+	fmt.Println(removeDuplicates(nums))
+}
