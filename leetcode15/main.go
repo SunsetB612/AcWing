88e4cd5a030255577977ade0ac7ac18597ffed93
SunsetB612 <10235101575@stu.ecnu.edu.cn>
2026-04-14 23:19:01 +0800

    leetcode15
    Signed-off-by: SunsetB612 <10235101575@stu.ecnu.edu.cn>

---
+++ leetcode15/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	n := len(nums)
+	res := [][]int{}
+
+	for i := 0; i < n-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		left := i + 1
+		right := n - 1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum == 0 {
+				res = append(res, []int{nums[i], nums[left], nums[right]})
+				left++
+				right--
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
+			} else if sum < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+	return res
+}
+
+func main() {
+	nums := []int{-1, 0, 1, 2, -1, -4}
+	fmt.Println(threeSum(nums))
+	nums = []int{0, 1, 1}
+	fmt.Println(threeSum(nums))
+	nums = []int{0, 0, 0}
+	fmt.Println(threeSum(nums))
+}
